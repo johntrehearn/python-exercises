6112b60a43a04abea2f522e60d4f94a0da28a66e
John Trehearn <john.trehearn@metropolia.fi>
2026-08-20 11:50:32 +0300

    ex4

--- mod03/exercise4.py
+++ mod03/exercise4.py
@@ -1,12 +1,12 @@
-num1 = input("Enter the first integer: ")
-num2 = input("Enter the second integer: ")
-num3 = input("Enter the second integer: ")
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
 
 
-sum_of_numbers = num1 + num2 + num3                                       
-product_of_numbers  = float(num1 + num2 + num3 )
+sum_of_numbers = int(num1 + num2 + num3)                                       
+product_of_numbers  = int(num1 * num2 * num3 )
 average_of_numbers = int(sum_of_numbers) / 3
 
-print("The sum of the numbers: " + sum_of_numbers)
-print("The product of the numbers: " + product_of_numbers)
-print("The average of the numbers: " + average_of_numbers)
+print("The sum of the numbers: " + str(sum_of_numbers))
+print("The product of the numbers: " + str(product_of_numbers))
+print("The average of the numbers: " + str(average_of_numbers))
