95a68e43ebc4fa0b6acb33d5b4ae55cde4db824c
John Trehearn <john.trehearn@metropolia.fi>
2026-08-25 15:00:16 +0300

    Project 1 added

---
+++ mod04/if-elif.py
@@ -0,0 +1,18 @@
+name = input("Enter name: ")
+
+if name == "Ava":
+    print("You are a teacher, teaching Tuesday")
+elif name == "Juha":
+    print("You are a teacher, teaching Wednesday")
+else:
+    print("You are a student or something else")
+
+if name == "Ava" or name == "Juha":
+    print("You are a teacher")
+
+# single line comment
+
+"""     Multiline
+        Commands"""
+
+# Alt shift A     or   Ctrl l
---
+++ project/startPage.py
@@ -0,0 +1,4 @@
+playerName = input("Please enter your name: ")
+playerAge = input("Please enter your age: ")
+
+print(f"\nHello {playerName}, your age is {playerAge}\n")
