3ebef63427f68fe0eeb059124db9054de45ec838
John Trehearn <john.trehearn@metropolia.fi>
2026-08-25 13:33:23 +0300

    Ex5

---
+++ mod03/exercise5.py
@@ -0,0 +1,21 @@
+talents = float(input("Enter talents: "))
+pounds = float(input("Enter pounds: "))
+lots = float(input("Enter lots: "))
+
+luoti = lots
+
+talentsToPounds = talents * 20
+talentsToLots = talentsToPounds * 32
+talentsToGrams = talentsToLots * 13.3
+
+poundsToLots = pounds * 32
+poundsToGrams = poundsToLots *13.3
+lotsToGrams = lots * 13.3
+
+total_grams = talentsToGrams + poundsToGrams + lotsToGrams
+
+kilograms = int(total_grams // 1000)
+remaining_grams = total_grams % 1000
+
+print("The weight in modern units:")
+print(f"{kilograms} kilograms and {remaining_grams:.2f} grams.")
