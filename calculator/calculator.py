09d0466bccb9b170239ec1cdc8cd6d8604429bd1
John Trehearn <john.trehearn@metropolia.fi>
2026-08-26 11:35:38 +0300

    calc my go

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,21 +1,37 @@
-calculation = input("Please enter the number for the calculation type you want \n 1. Add \n 2. Minus \n 3. Multiply \n 4. Divide \n 5. Exit\n")
+calculationType = input("Please enter the number for the calculationType type you want \n 1. Add \n 2. Minus \n 3. Multiply \n 4. Divide \n 5. Exit\n")
 
 question1 = ("Please enter first number: ")
 question2 = ("Please enter second number: ")
 
-while input != 5:
+while calculationType != "5":
 
-    if calculation == "1":
+    if calculationType == "1":
         number1 = int(input(question1))
         number2 = int(input(question2))
         print(f"Your two numbers were {number1} and {number2}")
         result = number1 + number2
-        print(f"The calculation result is: {result}")
-    elif calculation == "2":
+        print(f"The calculationType result is: {result}\n")
+    elif calculationType == "2":
+        number1 = int(input(question1))
+        number2 = int(input(question2))
+        print(f"Your two numbers were {number1} and {number2}")
+        result = number1 - number2
+        print(f"The calculationType result is: {result}\n")
+    elif calculationType == "3":
+        number1 = int(input(question1))
+        number2 = int(input(question2))
+        print(f"Your two numbers were {number1} and {number2}")
+        result = number1 * number2
+        print(f"The calculationType result is: {result}\n")
         print("two selected")
-    elif calculation == "3":
-        print("three selected")
-    elif calculation == "4":
-        print("four selected")
+    elif calculationType == "4":
+        number1 = int(input(question1))
+        number2 = int(input(question2))
+        print(f"Your two numbers were {number1} and {number2}")
+        result = number1 / number2
+        print(f"The calculationType result is: {result}\n")
     else:
         print("Please enter a valid choice!")
+        # To actually ask the question again, you need to re-assign it to a new input().
+    calculationType = input("Please enter the number for the calculationType type you want \n 1. Add \n 2. Minus \n 3. Multiply \n 4. Divide \n 5. Exit\n")
+print("Goodbye!")
