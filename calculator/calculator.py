88c2b12682ebe45671e7041df29637d323955373
John Trehearn <john.trehearn@metropolia.fi>
2026-08-26 10:34:10 +0300

    calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,12 @@
+calculation = input("Please enter the number for the calculation type you want \n 1. Add \n 2. Minus \n 3. Multiply \n 4. Divide \n")
+
+if calculation == "1":
+   print("one selected")
+elif calculation == "2":
+   print("two selected")
+elif calculation == "3":
+   print("three selected")
+elif calculation == "4":
+   print("four selected")
+else:
+   print("Please enter a valid choice!")
