ca2965fbe8b4fe892a93f244761e7ccd905325f3
John Trehearn <john.trehearn@metropolia.fi>
2026-08-27 09:48:45 +0300

    moved full menu into while loop

--- project/startPage.py
+++ project/startPage.py
@@ -6,20 +6,20 @@ if playerAge < 12:
     print("\nSorry but you are a minor\n\nGoodbye for now\n")
 else:
     print(f"\nHello {playerName}, your age is {playerAge}\n")
-    print("**Main Menu**\n\n1. Play the game\n2. Instructions\n3. Options")
 
     menuChoice ="0"
 
     while menuChoice != "lopeta":
 
+        print("**Main Menu**\n\n1. Play the game\n2. Instructions\n3. Options")
         menuChoice = input("\nPlease choose a menu item or Enter \"lopeta\" to exit\n")
 
         if menuChoice == "1":
-            print("Menu 1 selected")
+            print("\nWelcome to the game.\n\nThe Adventure Awaits\n")
         elif menuChoice == "2":
-            print("Menu 2 selected")
+            print("\n**The Termainal Castle game**\n\n -Read the text \n -Let us know what you want to do.\n")
         elif menuChoice == "3":
-            print("Menu 3 selected")
+            print("\nLots of great in game options here.\n")
         else:
             if menuChoice != "lopeta":
                 print("Incorrect option selected")
