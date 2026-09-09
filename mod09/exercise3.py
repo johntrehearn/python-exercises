7d73a94fb1694d5567766dff24e2c0f14d1d3dc5
johntrehearn <johntrehearnbc@gmail.com>
2026-09-09 12:02:06 +0300

    Ex03

---
+++ mod09/exercise3.py
@@ -0,0 +1,24 @@
+class Car:
+    def __init__(self, reg_number, max_speed):
+        self.license_plate = reg_number
+        self.maximum_speed = max_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate(self, change):
+        if self.current_speed + change >= self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        elif self.current_speed + change <= 0:
+            self.current_speed = 0
+        else:
+            self.current_speed += change
+
+    def drive(self, hours):
+        self.travelled_distance += hours * self.current_speed
+        
+car1 = Car("ABC-123", 142)
+car1.accelerate(60)
+
+car1.drive(1.5)
+
+print(f"Distance after driving 1.5 hours at 60 km/h: {car1.travelled_distance} km")
