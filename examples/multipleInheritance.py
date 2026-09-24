e618c20d45d0f55f2686704b084ef61ee419c5a2
John Trehearn <john.trehearn@metropolia.fi>
2026-09-24 13:54:16 +0300

    Mod10 ex5

--- examples/multipleInheritance.py
+++ examples/multipleInheritance.py
@@ -9,6 +9,9 @@ class SportsItem:
 # We cannot use Super here because we have multiple inheritance. We need to call the constructors of both parent classes explicitly.
 # The super() function in Python is used to access and call methods from a parent (or sibling) class. It allows you to inherit functionality 
 # without explicitly naming the parent class, making your code more flexible, maintainable, and adaptable to changes.
+# so you use the super() function when you have a single inheritance hierarchy, and you want to call a method from the parent class without hardcoding its name.
+# self represents the specific instance of the class that you are currently creating or manipulating.
+
 class Bicycle(Vehicle, SportsItem):
     def __init__(self, speed, weight, gears):
         Vehicle.__init__(self, speed)
